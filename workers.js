94da88f57fefe8602878d9edc513190ef0fcb880
Jie Liu <liujie333333@gmail.com>
2020-08-06 15:06:05 +0800

    Update workers.js

--- workers.js
+++ workers.js
@@ -1,6 +1,6 @@
 async function handleRequest(request) {
   const ip = request.headers.get("cf-connecting-ip") 
-  return new Response(ip,{status: 200})
+  return new Response(ip, {status: 200})
 }
 addEventListener('fetch', event => {
   return event.respondWith(handleRequest(event.request))
